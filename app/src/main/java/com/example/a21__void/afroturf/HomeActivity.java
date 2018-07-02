bf94e312f1f6df356581576c47de3bd830feab96
JeffDropsIT <mathonsiphumzile@gmail.com>
2018-07-02 14:18:53 -0200

    current errors

--- app/src/main/java/com/example/a21__void/afroturf/HomeActivity.java
+++ app/src/main/java/com/example/a21__void/afroturf/HomeActivity.java
@@ -1,22 +1,74 @@
 package com.example.a21__void.afroturf;
 
+import android.content.pm.PackageManager;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
 
-public class HomeActivity extends AppCompatActivity {
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class HomeActivity extends AppCompatActivity implements OnMapReadyCallback {
 
 
-
-
-    private GeofencingClient mGeofencingClient;
+    LocationManager locationManager;
+    MyLocationListiner locationListener;
+    private TextView ttvUpdate;
 
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)  {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
 
-        mGeofencingClient = LocationServices.getGeofencingClient(this);
+
+        // Get the SupportMapFragment and request notification
+        // when the map is ready to be used.
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+        Log.i("asd", "onCreate: ");
+        locationManager = (LocationManager) getSystemService(this.LOCATION_SERVICE);
+        locationListener = new MyLocationListiner(getApplicationContext());
+        if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 5000, 2, locationListener);
+        
+        
+        ttvUpdate = findViewById(R.id.ttv_update);
+        
+        ttvUpdate.setText(locationListener.getUpdate());
+
+
+
+
+
+
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+
+        LatLng sydney = new LatLng(-33.852, 151.211);
+        googleMap.addMarker(new MarkerOptions().position(sydney)
+                .title("Marker in Sydney"));
+        googleMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
     }
 }
---
+++ app/src/main/java/com/example/a21__void/afroturf/MyLocationListiner.java
@@ -0,0 +1,86 @@
+package com.example.a21__void.afroturf;
+
+import android.content.Context;
+import android.location.Address;
+import android.location.Geocoder;
+import android.location.Location;
+import android.location.LocationListener;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Locale;
+
+
+class MyLocationListiner implements LocationListener{
+
+
+    public static final String TAG = "asd";
+    private final Context context;
+
+    private String update;
+    private String cityName;
+
+    private Geocoder geocoder;
+
+    List<Address> addresses;
+
+    public MyLocationListiner(Context context){
+        this.context = context;
+    }
+    @Override
+    public void onLocationChanged(Location loc) {
+
+
+        String longitude = "Longitude: " + loc.getLongitude();
+        Log.v(TAG, longitude);
+        String latitude = "Latitude: " + loc.getLatitude();
+        Log.v(TAG, latitude);
+
+        update = latitude+" lat | long "+longitude;
+
+        geocoder = new Geocoder(context, Locale.getDefault());
+
+
+        try {
+            addresses = geocoder.getFromLocation(loc.getLatitude(),
+                    loc.getLongitude(), 1);
+            if (addresses.size() > 0) {
+                System.out.println(addresses.get(0).getLocality());
+                cityName = addresses.get(0).getLocality();
+            }
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+        }
+        update = longitude + "\n" + latitude;
+
+        Toast.makeText(context, update, Toast.LENGTH_SHORT).show();
+
+
+
+
+
+    }
+
+    public String getUpdate() {
+        return update;
+    }
+
+    @Override
+    public void onStatusChanged(String provider, int status, Bundle extras) {
+
+    }
+
+    @Override
+    public void onProviderEnabled(String provider) {
+
+    }
+
+    @Override
+    public void onProviderDisabled(String provider) {
+
+    }
+}
